2251b9db049671af78b8cc274a5e9089357a0f44
nacrt <37629791+nacrt@users.noreply.github.com>
2019-04-10 19:35:23 +0200

    updated .gitignore

--- MakingDiscordGreater/frmLargeImages.cs
+++ MakingDiscordGreater/frmLargeImages.cs
@@ -16,7 +16,6 @@ namespace MDG
 		/// makes sure you can open it again :)
 		private void images_closed(object sender, FormClosedEventArgs e) => Main.opendLarge = false;
 
-		//hi
 		private static long client_id;
 		private static string folder_path;
 		private static string file_name = "largeImagesIndex.txt";
